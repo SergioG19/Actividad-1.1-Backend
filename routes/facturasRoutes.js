48d44d6defc44eb855f283acc16a1385bbbf0376
Maria Suarez <suarezaraujomaal@uvm.edu.ve>
2023-06-22 21:35:17 -0400

    19 commit
    Se agregaron, mejoraron y actualizaron algunas rutas para un mejor funcionamiento

--- routes/facturasRoutes.js
+++ routes/facturasRoutes.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 const facturasController = require('../controllers/facturasController');
 
+router.get('/vista', (req, res) => {
+    res.render('facturas');
+});
+
 router.post('/', (req, res) => {
   facturasController.agregar(req, res);
 });
 
 router.get('/', (req, res) => {
-  facturasController.listar(req, res);
+  const facturas = facturasController.listar();
+  res.json(facturas);
 });
 
 router.put('/:id', (req, res) => {
