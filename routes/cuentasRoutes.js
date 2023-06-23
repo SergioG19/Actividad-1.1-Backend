1bae2aefc1ba55620451da262fe6c70366058dbb
Yorkina Gonzalez <gonzalezpachecoyv@uvm.edu.ve>
2023-06-22 21:02:04 -0400

    16 commit
    Se actualizó este código para modificar algunas rutas y así mejorar el funcionamiento con junto al html de cuenta.ejs

--- routes/cuentasRoutes.js
+++ routes/cuentasRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const cuentasController = require('../controllers/cuentasController');
+const CuentasController = require('../controllers/cuentasController');
+const cuentasController = new CuentasController(); 
 
 router.post('/', (req, res) => {
   cuentasController.agregar(req, res);
 });
 
 router.get('/', (req, res) => {
-  cuentasController.listar(req, res);
+  const cuentas = cuentasController.listar();
+  res.json(cuentas);
 });
 
 router.put('/:id', (req, res) => {
@@ -16,6 +18,11 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   cuentasController.eliminar(req, res);
+});
+
+router.get('/vista', (req, res) => {
+  const cuentas = cuentasController.listar(req, res);
+  res.render('cuentas', { cuentas });
 });
 
 module.exports = router;
