5b6b93b1bad73342580d8c0c636abb263af9f347
Yorkina Gonzalez <gonzalezpachecoyv@uvm.edu.ve>
2023-06-22 20:58:32 -0400

    15 commit
    Se modificó varias cosas de este codigo para que pueda funcionar con el codigo html de cuenta.ejs

--- controllers/cuentasController.js
+++ controllers/cuentasController.js
@@ -4,24 +4,30 @@ class CuentasController {
   }
 
   agregar(req, res) {
-    const cuenta = req.body;
+    const cuenta = {
+      id: req.body.id,
+      nombre: req.body.nombre,
+      tipo: req.body.tipo,
+    };
+
     this.cuentas.push(cuenta);
     res.status(201).json(cuenta);
   }
 
-  listar(req, res) {
-    res.json(this.cuentas);
+  listar() {
+    return this.cuentas;
   }
 
   editar(req, res) {
     const id = parseInt(req.params.id);
     const cuenta = req.body;
     const index = this.cuentas.findIndex((c) => c.id === id);
-
+  
     if (index === -1) {
       res.status(404).json({ message: 'Cuenta no encontrada' });
     } else {
-      this.cuentas[index] = cuenta;
+      this.cuentas.splice(index, 1);
+      this.cuentas.push(cuenta);
       res.json(cuenta);
     }
   }
@@ -39,4 +45,4 @@ class CuentasController {
   }
 }
 
-module.exports = new CuentasController();
+module.exports = CuentasController; 
